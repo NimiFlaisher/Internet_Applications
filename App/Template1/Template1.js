1581e18b92d260dd9bb88961ad5d836ac63ff813
Nimi <Nimrobo@Gmail.com>
2022-04-14 16:29:35 +0300

    check

--- App/Template1/Template1.js
+++ App/Template1/Template1.js
@@ -1,12 +1,28 @@
+const currentAd = localStorage.getItem("adNum");
+console.log(currentAd);
+
 function addElements () {
-    for (let i = 0; i < ads.ad_2.photos.length; i++) {
+    console.log(ads[currentAd].photos.length);
+    if (ads[currentAd].photos.length != 0)
+    for (let i = 0; i < ads[currentAd].photos.length; i++) {
         const newDiv = document.createElement("div");
         newDiv.className= "option";
-        newDiv.style.backgroundImage ="url('"+ads.ad_2.photos[i]+"')"
+        newDiv.style.backgroundImage ="url('"+ads[currentAd].photos[i]+"')";
       
         const currentDiv = document.getElementById("div1");
         document.getElementById("opt").insertBefore(newDiv, currentDiv);
     }
+
+    if (ads[currentAd].texts.length != 0)
+    for (let i = 0; i < ads[currentAd].texts.length; i++) {
+        const newDiv = document.createElement("span");
+        newDiv.className= "textLine";
+        newDiv.textContent = ads[currentAd].texts[i];
+      
+        const currentDiv = document.getElementById("span1");
+        document.getElementById("text").insertBefore(newDiv, currentDiv);
+    }
+
 }
 
 addElements();
