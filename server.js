fd9f670d97b99166ee792412bed9ce908c0ee5d3
Nimi <Nimrobo@Gmail.com>
2022-05-16 18:55:41 +0300

    Added slash logic, data transfer, and ads array that corresponds with the active screen numbers

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require ('express');
 var path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/public'));
+const ads = require("./data");
+let data;
+
+const getData = (screenNumber) => { // << the ads array (with active screen number)
+  return ads.filter((item) => item.screens.includes(screenNumber));
+};
 
 app.get('/', function(req, res){
     var options = {
@@ -16,6 +22,17 @@ app.get('/', function(req, res){
             console.log('Sent:', fileName);
         }
     });
+});
+
+app.get("/", function (req, res) { // צריך לבדוק אם זה יעבוד בגלל שכבר יש לנו סלאש אחד מעל
+  if (req?.query && req.query?.screen) {
+    data = getData(Number(req.query.screen));
+    res.sendFile(__dirname + "/index.html");
+  } else res.send("please specify a screen");
+});
+
+app.get("/data", function (req, res) { // שולח את המערך
+  res.send(data);
 });
 
 app.get("/Loading/Loading.html", function (req, res) {
